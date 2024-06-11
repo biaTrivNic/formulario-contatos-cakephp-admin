37a70e9bfb8fc36bf523922bbc1f297c556f6308
Beatriz Trivillin <beatriztrivillin@nic.br>
2024-06-11 09:09:23 -0300

    alterando database

--- sql/database.sql
+++ sql/database.sql
@@ -4,11 +4,10 @@ CREATE TABLE Messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    mensagem TEXT,
-    data_envio DATETIME NOT NULL
+    mensagem TEXT
 );
 
-INSERT INTO Messages (nome, email, mensagem, data_envio) VALUES
-('João Silva', 'joao.silva@example.com', 'Olá, esta é uma mensagem de teste.', '2024-06-03 10:00:00'),
-('Maria Oliveira', 'maria.oliveira@example.com', 'Aqui está outra mensagem de teste.', '2024-06-03 10:05:00'),
-('Carlos Souza', 'carlos.souza@example.com', 'Mensagem de teste enviada por Carlos.', '2024-06-03 10:10:00');
+INSERT INTO Messages (nome, email, mensagem) VALUES
+('João Silva', 'joao.silva@example.com', 'Olá, esta é uma mensagem de teste.'),
+('Maria Oliveira', 'maria.oliveira@example.com', 'Aqui está outra mensagem de teste.'),
+('Carlos Souza', 'carlos.souza@example.com', 'Mensagem de teste enviada por Carlos.');
