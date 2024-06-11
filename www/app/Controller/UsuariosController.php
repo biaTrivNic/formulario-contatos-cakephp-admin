21ac8e7006e045abdfe8a97353ed2595d9b41c77
Beatriz Trivillin <beatriztrivillin@nic.br>
2024-06-11 09:07:06 -0300

    deletando arquivos

--- www/app/Controller/UsuariosController.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-class UsuariosController extends AppController {
-    public $helpers = array('Html', 'Form');
-
-    public function index() {
-        $this->set('usuarios', $this->Usuario->find('all'));
-    }
-}
