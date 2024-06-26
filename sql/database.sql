a9f1193fb61c53b8cc30f66396147bafd62c6411
Beatriz Trivillin <beatriztrivillin@nic.br>
2024-06-25 21:07:24 -0300

    criando tabela users

--- sql/database.sql
+++ sql/database.sql
@@ -7,7 +7,9 @@ CREATE TABLE Messages (
     mensagem TEXT
 );
 
-INSERT INTO Messages (nome, email, mensagem) VALUES
-('João Silva', 'joao.silva@example.com', 'Olá, esta é uma mensagem de teste.'),
-('Maria Oliveira', 'maria.oliveira@example.com', 'Aqui está outra mensagem de teste.'),
-('Carlos Souza', 'carlos.souza@example.com', 'Mensagem de teste enviada por Carlos.');
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
